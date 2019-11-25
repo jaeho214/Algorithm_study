889a8d6620c16e4b33e0eab31e75ebd0aa8925a7
jaeho214 <jaeho214@naver.com>
2019-11-25 19:19:06 +0900

    bfs study(11.25)

---
+++ src/bfs/BOJ_1679_숨바꼭질.java
@@ -0,0 +1,63 @@
+package bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_1679_숨바꼭질 {
+    private static int n, k;
+    private static boolean visited[];
+    //bfs로 하되 배열에 거리를 저장하면서 진행함
+    private static int road[];
+    public static void main(String[] args) {
+        input();
+        System.out.println(solve());
+    }
+
+    private static int solve() {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(n);
+        int a=0;
+        while(!q.isEmpty()){
+            a = q.poll();
+            int array[] = {1,-1, a};
+            visited[a] = true;
+
+            for(int i=0;i<3;i++){
+                int next = a + array[i];
+                //10만으로 해줬어야 했는데 10만1로 범위 설정 잘못해서 런타임 에러
+                if(next >= 0 && next <= 100000){
+                    if(next != k && !visited[next]){
+                        q.add(next);
+                        visited[next] = true;
+                        road[next] = road[a]+1;
+                    }
+                    else if(next == k && !visited[next]){
+                        road[next] = road[a]+1;
+                        q.clear();
+                        break;
+                    }
+                }
+            }
+        }
+        return road[k];
+    }
+
+    private static void input() {
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            visited = new boolean[100001];
+            road = new int[100001];
+            n = Integer.parseInt(st.nextToken());
+            k = Integer.parseInt(st.nextToken());
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/bfs/BOJ_7576_토마토.java
@@ -0,0 +1,96 @@
+package bfs;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_7576_토마토 {
+    private static boolean visited[][];
+    private static int a[][];
+    private static int n,m;
+    private static int dx[] = {0,1,0,-1};
+    private static int dy[] = {1,0,-1,0};
+    static Queue<Integer> qx;
+    static Queue<Integer> qy;
+
+    public static void main(String[] args) {
+        input();
+        //출발지 큐에서 하나를 꺼내서 시작한다.
+        int result = solve(qx.peek(),qx.peek());
+        output(result);
+    }
+
+    private static void output(int result) {
+        try{
+            String str = String.valueOf(result);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            bw.write(str);
+            bw.flush();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static int solve(int x, int y) {
+        while(!qx.isEmpty() && !qy.isEmpty()) {
+            x = qx.poll();
+            y = qy.poll();
+            visited[x][y] = true;
+            for (int i = 0; i < 4; i++) {
+                int nextX = x + dx[i];
+                int nextY = y + dy[i];
+                if (nextX > 0 && nextY > 0 && nextX <= n && nextY <= m) {
+                    if (a[nextX][nextY] == 0 && !visited[nextX][nextY]) {
+                        qx.add(nextX);
+                        qy.add(nextY);
+                        visited[nextX][nextY] = true;
+                        a[nextX][nextY] = a[x][y] + 1;
+                    }
+                }
+            }
+        }
+
+        int max = 0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(a[i][j] == 0){
+                    return -1;
+                }else {
+                    max = Math.max(a[i][j], max);
+                }
+            }
+        }
+
+        return max-1;
+
+    }
+
+    private static void input() {
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+
+            a = new int[n+1][m+1];
+            visited = new boolean[n+1][m+1];
+            qx = new LinkedList<>();
+            qy = new LinkedList<>();
+
+            for(int i=1;i<=n;i++){
+                StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+                for(int j=1;j<=m;j++){
+                    a[i][j] = Integer.parseInt(stringTokenizer.nextToken());
+                    if(a[i][j] == 1){
+                        //출발 지점을 미리 큐에 넣어놓음
+                        qx.add(i);
+                        qy.add(j);
+                    }
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
