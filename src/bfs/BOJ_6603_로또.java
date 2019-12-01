11d5e2a9ff14c5f7297aa8644a1819472254899d
jaeho214 <jaeho214@naver.com>
2019-12-02 00:03:08 +0900

    dfs study(12.01)

--- src/bfs/BOJ_6603_로또.java
+++ src/bfs/BOJ_6603_로또.java
@@ -3,8 +3,6 @@ package bfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ_6603_로또 {
