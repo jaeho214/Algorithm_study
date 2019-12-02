43a3d41159e6d1cfbb7ac1c50a3347c1acb007d1
jaeho214 <jaeho214@naver.com>
2019-12-02 15:21:18 +0900

    dfs study(12.01)

--- src/bfs/BOJ_6603_로또.java
+++ src/bfs/BOJ_6603_로또.java
@@ -9,11 +9,14 @@ public class BOJ_6603_로또 {
     private static int k,cnt;
     private static int s[];
     private static StringBuilder sb = new StringBuilder();
+    private static boolean button = true;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        while(!br.readLine().equals("0")){
+        while(button){
             StringTokenizer st = new StringTokenizer(br.readLine());
             k = Integer.parseInt(st.nextToken());
+            if(k == 0)
+                button = false;
             s = new int[k+1];
             for(int i=1;i<=k;i++){
                 s[i] = Integer.parseInt(st.nextToken());
@@ -27,11 +30,12 @@ public class BOJ_6603_로또 {
         System.out.println(sb.toString());
     }
 
+    //백트래킹 방식
     public static void dfs(int v, String str) {
         if (cnt == 6) {
             sb.append(str + "\n");
         } else {
-            for (int i = v + 1; i <= k; i++) {
+            for (int i = v+1; i <= k; i++) {
                 ++cnt;
                 dfs(i, str + s[i] + " ");
             }
