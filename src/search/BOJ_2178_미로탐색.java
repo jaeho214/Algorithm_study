8c7941c3099700811787b33dc0a22a3e5b7c24cb
jaeho214 <jaeho214@naver.com>
2019-12-04 19:50:19 +0900

    bfs study(11.25)

--- src/search/BOJ_2178_미로탐색.java
+++ src/search/BOJ_2178_미로탐색.java
@@ -1,4 +1,4 @@
-package bfs;
+package search;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- src/search/BOJ_7576_토마토.java
+++ src/search/BOJ_7576_토마토.java
@@ -1,4 +1,4 @@
-package bfs;
+package search;
 
 import java.io.*;
 import java.util.LinkedList;
