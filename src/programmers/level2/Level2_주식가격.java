e46c30d985bcb32d4a7aba483eb3cc1fc1a7f184
jaeho214 <jaeho214@naver.com>
2020-08-25 00:29:53 +0900

    level2 study(08.24)

--- src/programmers/level2/Level2_주식가격.java
+++ src/programmers/level2/Level2_주식가격.java
@@ -1,33 +1,28 @@
 package programmers.level2;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-
 public class Level2_주식가격 {
     public static void main(String[] args) {
         Level2_주식가격 level2_주식가격 = new Level2_주식가격();
-        int[] price = {1,2,3,2,3,1};
+        int[] price = {1, 2, 3, 2, 3};
         System.out.println(level2_주식가격.solution(price));
     }
 
     public int[] solution(int[] prices) {
-        Queue<Integer> q = new LinkedList<>();
-        List<Integer> answer = new ArrayList<>();
-        for(int i : prices){
-            q.add(i);
-        }
-        while(!q.isEmpty()){
-            int num = q.poll();
-            int cnt = 0;
-            for (Integer i : q) {
-                cnt++;
-                if(num > i)
+        int[] answer = new int[prices.length];
+
+        for(int i=0;i< prices.length;i++){
+            int j;
+            for(j=i+1;j< prices.length;j++){
+                if(prices[i] > prices[j]){
+                    answer[i] = j-i;
                     break;
+                }else{
+                    answer[i] = j-i;
+                }
             }
-            answer.add(cnt);
+
         }
-        return answer.stream().mapToInt(i -> i).toArray();
+
+        return answer;
     }
 }
